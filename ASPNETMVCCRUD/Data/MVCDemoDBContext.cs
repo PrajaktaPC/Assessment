d8d940c7f9d764e5adac4da8b199b49e4068484b
PrajaktaPC <chaudhari.prajakta4@gmail.com>
2023-11-06 09:00:30 +0530

    second Commit

---
+++ ASPNETMVCCRUD/Data/MVCDemoDBContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using ASPNETMVCCRUD.Models.Domain;
+
+namespace ASPNETMVCCRUD.Data
+{
+    public class MVCDemoDBContext : DbContext
+    {
+        public MVCDemoDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
